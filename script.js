4ca9c95dbfef4f7e99c820ac89d8daa60c26f94e
João Victor Pistorio <joao.pistorio@gmail.com>
2021-03-09 17:11:18 -0300

    Correcao requisito 3

---
+++ script.js
@@ -0,0 +1,8 @@
+let board = document.getElementById('pixel-board');
+let newpixel = document.createElement('div');
+console.log(`Board: ${board}`);
+newpixel.className = 'pixel';
+board.append(newpixel);
+
+
+
