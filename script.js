7ae40f565b584fc3b819ef6975a9a51ae7c12183
João Victor Pistorio <joao.pistorio@gmail.com>
2021-03-10 00:44:55 -0300

    Requisito 10 correcao

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
 const board = document.getElementById('pixel-board');
-// const linha1 = document.createElement('div');
-// const linha2 = document.createElement('div');
-// const linha3 = document.createElement('div');
-// const linha4 = document.createElement('div');
-// const linha5 = document.createElement('div');
+const linha1 = document.createElement('div');
+const linha2 = document.createElement('div');
+const linha3 = document.createElement('div');
+const linha4 = document.createElement('div');
+const linha5 = document.createElement('div');
 
-// linha1.className = 'linha1';
-// linha2.className = 'linha2';
-// linha3.className = 'linha3';
-// linha4.className = 'linha4';
-// linha5.className = 'linha5';
+linha1.className = 'linha1';
+linha2.className = 'linha2';
+linha3.className = 'linha3';
+linha4.className = 'linha4';
+linha5.className = 'linha5';
 
-// board.appendChild(linha1);
-// board.appendChild(linha2);
-// board.appendChild(linha3);
-// board.appendChild(linha4);
-// board.appendChild(linha5);
+board.appendChild(linha1);
+board.appendChild(linha2);
+board.appendChild(linha3);
+board.appendChild(linha4);
+board.appendChild(linha5);
 
-// const boardtest = document.getElementById('board-teste');
+for (let j = 0; j < 5; j += 1) {
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  linha1.appendChild(pixel);
+}
 
-// for (let j = 0; j < 5; j += 1) {
-//   const pixel = document.createElement('div');
-//   pixel.className = 'pixel';
-//   linha1.appendChild(pixel);
-// }
+for (let j = 0; j < 5; j += 1) {
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  linha2.appendChild(pixel);
+}
 
-// for (let j = 0; j < 5; j += 1) {
-//   const pixel = document.createElement('div');
-//   pixel.className = 'pixel';
-//   linha2.appendChild(pixel);
-// }
+for (let j = 0; j < 5; j += 1) {
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  linha3.appendChild(pixel);
+}
 
-// for (let j = 0; j < 5; j += 1) {
-//   const pixel = document.createElement('div');
-//   pixel.className = 'pixel';
-//   linha3.appendChild(pixel);
-// }
+for (let j = 0; j < 5; j += 1) {
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  linha4.appendChild(pixel);
+}
 
-// for (let j = 0; j < 5; j += 1) {
-//   const pixel = document.createElement('div');
-//   pixel.className = 'pixel';
-//   linha4.appendChild(pixel);
-// }
-
-// for (let j = 0; j < 5; j += 1) {
-//   const pixel = document.createElement('div');
-//   pixel.className = 'pixel';
-//   linha5.appendChild(pixel);
-// }
+for (let j = 0; j < 5; j += 1) {
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  linha5.appendChild(pixel);
+}
 
 const color1 = document.getElementById('one');
 const color2 = document.getElementById('two');
@@ -150,6 +148,7 @@ function generateBoard(value) {
     for (let j = 1; j <= value; j += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
+      pixel.style.backgroundColor = 'white';
       linha.appendChild(pixel);
     }
   }
